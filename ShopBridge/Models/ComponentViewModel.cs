0fefbe7a77758edfc7e3da2937a36907b478ef29
Akshay Jadhav <jadhavakshay4124@gmail.com>
2020-09-08 15:54:01 +0530

    Add project files.

---
+++ ShopBridge/Models/ComponentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBridge.Models
+{
+    public class ComponentViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
